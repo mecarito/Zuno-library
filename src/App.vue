e2cfaabc5c2f52935964436a1949a33088f20c7e
mecarito <pieritomecar@gmail.com>
2023-03-17 15:55:13 +0300

    deleted default view files and updated root view

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,19 @@
 <script setup lang="ts"></script>
 
 <template>
-  <main class="bg-bg-light h-headerBg">
-    <header>
-      <div class="flex flex-row items-center">
-        <img alt="Zuno library logo" src="@/assets/logo.svg" class="w-16 h-20" />
-        <p class="capitalize text-header-text text-2xl italic">Zuno library</p>
-      </div>
-      <img
-        alt="a man's face"
-        src="https://randomuser.me/api/portraits/med/men/75.jpg"
-        class="w-8 h-8 rounded-full"
-      />
-    </header>
-    <div></div>
-  </main>
+  <header class="bg-bg-light h-headerBg">
+    <div class="flex flex-row items-center">
+      <img alt="Zuno library logo" src="@/assets/logo.svg" class="w-16 h-20" />
+      <p class="capitalize text-header-text text-2xl italic">Zuno library</p>
+    </div>
+    <img
+      alt="a man's face"
+      src="https://randomuser.me/api/portraits/med/men/75.jpg"
+      class="w-8 h-8 rounded-full"
+    />
+  </header>
+  <main></main>
+  <footer></footer>
 </template>
 
 <style scoped></style>
